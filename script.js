70dab87abe8235c4f495d82d1e2f7dc0b29b4faf
Gustavo Gomes <144964704+Gustavoggomesdev@users.noreply.github.com>
2025-05-03 09:46:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -54,3 +54,12 @@ function toggleAudio() {
   playing = !playing;
 }
 
+
+window.addEventListener('scroll', function () {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
